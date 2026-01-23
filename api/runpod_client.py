22519c4310054a5f3ca9cf229298437082e1fbfd
Waleed Ali <waleedali09az@gmail.com>
2026-01-23 10:41:52 +0500

    remove audio conversion to wav now send it as browser recoring it (webm)

--- api/runpod_client.py
+++ api/runpod_client.py
@@ -8,7 +8,7 @@ import asyncio
 # ===============================
 
 ENDPOINT_ID = os.getenv("RUNPOD_ENDPOINT_ID")
-BASE_URL = f"https://btfyb0763mvyks-8000.proxy.runpod.net"
+BASE_URL = f"https://gyjmkiojvt0l74-8000.proxy.runpod.net"
 
 HEADERS = {
     "Content-Type": "application/json",
